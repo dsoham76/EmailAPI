ee323db5678f9bcd8816cd4394af8f1ffd8a4908
Soham <dsoham76@gmail.com>
2023-06-18 14:02:42 +0530

    Adding email-sender

---
+++ email-sender/src/main/java/com/example/emailsender/EmailController.java
@@ -0,0 +1,31 @@
+package com.example.emailsender.controller;
+
+import com.example.emailsender.resource.EmailMessage;
+import com.example.emailsender.service.EmailSenderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    private final EmailSenderService emailSenderService;
+
+    public EmailController(EmailSenderService emailSenderService) {
+        this.emailSenderService = emailSenderService;
+    }
+
+    @PostMapping("/send-email")
+    public ResponseEntity sendEmail(@RequestBody EmailMessage emailMessage){
+        this.emailSenderService.sendEmail(emailMessage.getTo(),emailMessage.getSubject(),emailMessage.getMessage());
+        return ResponseEntity.ok("Success");
+
+    }
+
+    @GetMapping("/details")
+    public String getDetails(){
+        return  "your details";
+    }
+}
